d0ca94c161c1bed4df31f2bcce951c76327841f3
Pedro Magno <pedromagnopro@gmail.com>
2024-08-06 14:43:05 -0300

    chore: remove um import desnecessário da classe APIConfig

--- src/main/java/br/com/pedromagno/authorization/config/APIConfig.java
+++ src/main/java/br/com/pedromagno/authorization/config/APIConfig.java
@@ -1,6 +1,5 @@
 package br.com.pedromagno.authorization.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
